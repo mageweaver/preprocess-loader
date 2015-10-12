db08730e959fbd75438647786ef7fcc53bb7b553
Jared Spigner <jaredspigner@gmail.com>
2015-10-12 14:41:47 +0100

    updated macro capability and added additional checks

--- preprocess-loader.js
+++ preprocess-loader.js
@@ -58,15 +58,27 @@ module.exports = function(source) {
         line = line.replace(new RegExp('__FILE__'),""+fileName,'g');
       }
 
-      if(opt.config!==undefined) {
-        for(var x=0;x<opt.cfg.macros.length;x++) {
-          if(opt.cfg.macros[x].scope=="line") {
-            if(opt.cfg.macros[x].fileName=="all" || opt.cfg.macros[x].fileName==fileName) {
-              line = line.replace(new RegExp(opt.cfg.macros[x].regex),opt.cfg.macros[x].value,opt.cfg.macros[x].flags);
+      if(opt.config!==undefined && opt.cfg!==undefined && opt.cfg.regexes!==undefined) {
+        for(var x=0;x<opt.cfg.regexes.length;x++) {
+          if(opt.cfg.regexes[x].scope=="line") {
+            if(opt.cfg.regexes[x].fileName=="all" || opt.cfg.regexes[x].fileName==fileName) {
+              line = line.replace(new RegExp(opt.cfg.regexes[x].regex),opt.cfg.regexes[x].value,opt.cfg.regexes[x].flags);
             }
           }
         }
+      }
 
+      if(opt.config!==undefined && opt.cfg!==undefined && opt.cfg.macros!==undefined) {
+        for(var x=0;x<opt.cfg.macros.length;x++) {
+          if(opt.cfg.macros[x].scope=="line") {
+            if(opt.cfg.macros[x].fileName=="all" || opt.cfg.macros[x].fileName==fileName) {
+              line = line.replace(new RegExp(opt.cfg.macros[x].name),opt.cfg.macros[x].inline,opt.cfg.macros[x].flags);
+            }
+          }
+        }
+      } 
+
+      if(opt.config!==undefined && opt.cfg!==undefined && opt.cfg.callbacks!==undefined) {
         for(var x=0;x<opt.cfg.callbacks.length;x++) {
           if(opt.cfg.callbacks[x].scope=="line" && (opt.cfg.callbacks[x].fileName=="all" || opt.cfg.callbacks[x].fileName==fileName)) {
             var cb = eval(opt.cfg.callbacks[x].callback);
@@ -81,15 +93,27 @@ module.exports = function(source) {
     }
   }
 
-  if(opt.config!==undefined) {
-    for(var i=0;i<opt.cfg.macros.length;i++) {
-      if(opt.cfg.macros[i].scope=="source") {
-        if(opt.cfg.macros[i].fileName=="all" || opt.cfg.macros[i].fileName==fileName) {
-          content = content.replace(new RegExp(opt.cfg.macros[i].regex),opt.cfg.macros[i].replace-value,opt.cfg.macros[i].replace-scope);
+  if(opt.config!==undefined && opt.cfg!==undefined && opt.cfg.regexes!==undefined) {
+    for(var i=0;i<opt.cfg.regexes.length;i++) {
+      if(opt.cfg.regexes[i].scope=="source") {
+        if(opt.cfg.regexes[i].fileName=="all" || opt.cfg.regexes[i].fileName==fileName) {
+          content = content.replace(new RegExp(opt.cfg.regexes[i].regex),opt.cfg.regexes[i].replace-value,opt.cfg.regexes[i].replace-scope);
         }
       }
     }
+  }
 
+  if(opt.config!==undefined && opt.cfg!==undefined && opt.cfg.macros!==undefined) {
+    for(var x=0;x<opt.cfg.macros.length;x++) {
+      if(opt.cfg.macros[x].scope=="source") {
+        if(opt.cfg.macros[x].fileName=="all" || opt.cfg.macros[x].fileName==fileName) {
+          content = content.replace(new RegExp(opt.cfg.macros[x].name),opt.cfg.macros[x].inline,opt.cfg.macros[x].flags);
+        }
+      }
+    }
+  }
+
+  if(opt.config!==undefined && opt.cfg!==undefined && opt.cfg.callbacks!==undefined) {
     for(var i=0;i<opt.cfg.callbacks.length;i++) {
       if(opt.cfg.callbacks[i].scope=="source" && (opt.cfg.callbacks[i].fileName=="all" || opt.cfg.callbacks[i].fileName==fileName)) {
         var cb = eval(opt.cfg.callbacks[i].callback);
